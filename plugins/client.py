5ab1cec9ea9811f42716bd737990989f48652ffd
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-24 14:34:20 +0530

    Update client.py

--- plugins/client.py
+++ plugins/client.py
@@ -6,7 +6,7 @@ from typing import List, AsyncIterable
 from aiohttp import ClientSession
 from pathlib import Path
 
-from models import LastChapter
+from bot import LastChapter
 from tools import LanguageSingleton
 
 
