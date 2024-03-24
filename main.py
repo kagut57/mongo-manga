6a673ee2461b8bd0ca15cb5d9fd911698d308322
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-24 11:32:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,9 @@ from logger import logger
 from bot import bot, manga_updater, chapter_creation
 from models import mongodb
 
-async def async_main():
-    try:
-        db = mongodb()
-        await db.connect()
-    except Exception as e:
-        logger.error(f"Error connecting to the database: {e}")
-
 if __name__ == '__main__':
     loop = aio.get_event_loop_policy().get_event_loop()
-    loop.run_until_complete(async_main())
+    loop.run_until_complete(mongodb())
     loop.create_task(manga_updater())
     for i in range(10):
         loop.create_task(chapter_creation(i + 1))
