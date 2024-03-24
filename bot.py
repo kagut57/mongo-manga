c8961fc92c721f0b1cbc18c8d767fec59e6d5b51
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-24 16:39:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,6 +28,11 @@ from pagination import Pagination
 from plugins.client import clean
 from tools.aqueue import AQueue
 from tools.flood import retry_on_flood
+from collections import namedtuple
+
+LastChapter = namedtuple('LastChapter', ['url', 'chapter_url'])
+ChapterFile = namedtuple('ChapterFile', ['_id', 'file_id', 'file_unique_id', 'cbz_id', 'cbz_unique_id', 'telegraph_url'])
+
 
 mangas: Dict[str, MangaCard] = dict()
 chapters: Dict[str, MangaChapter] = dict()
