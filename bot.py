dd92f0bc0b2b5ca5861abfd0bebd607fc1045f9b
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-25 13:06:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -700,7 +700,7 @@ async def update_mangas():
                     if counter == 20:
                         break
                 if new_chapters:
-                    last_chapter.get("chapter_url") = new_chapters[0].url
+                    last_chapter.get("chapter_url") == new_chapters[0].url
                     last_chapter_dict = {
                         "url": url,
                         "chapter_url": new_chapters[0].url
