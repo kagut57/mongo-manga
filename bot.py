ae8c0079669c9be86a947c617e4f1c3a8ac42914
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-25 13:05:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -693,14 +693,14 @@ async def update_mangas():
                 new_chapters: List[MangaChapter] = []
                 counter = 0
                 async for chapter in client.iter_chapters(url, manga_name):
-                    if chapter.url == last_chapter.chapter_url:
+                    if chapter.url == last_chapter.get("chapter_url"):
                         break
                     new_chapters.append(chapter)
                     counter += 1
                     if counter == 20:
                         break
                 if new_chapters:
-                    last_chapter.chapter_url = new_chapters[0].url
+                    last_chapter.get("chapter_url") = new_chapters[0].url
                     last_chapter_dict = {
                         "url": url,
                         "chapter_url": new_chapters[0].url
