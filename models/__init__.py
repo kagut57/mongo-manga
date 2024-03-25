6aadb92518d759154184e020d281f46f6c04293c
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-03-25 12:50:14 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
 from .mongodb import mongodb
 from collections import namedtuple
 
-LastChapter = namedtuple("LastChapter", ["url", "chapter_url"])
+#LastChapter = namedtuple("LastChapter", ["url", "chapter_url"])
+class LastChapter:
+    def __init__(self, url: str, chapter_url: str):
+        self.url = url
+        self.chapter_url = chapter_url
+
